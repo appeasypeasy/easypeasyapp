1afaa2968ce6c4f2eab1b9e863bd4a5ed3827da5
Yeminson Sanchez <yeminsons@gmail.com>
2022-10-11 21:10:13 -0300

    config proyect

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,33 +6,21 @@ module.exports = {
     "jest/globals": true,
   },
   extends: [
-    "plugin:react/recommended",
     "standard",
     "eslint:recommended",
     "plugin:prettier/recommended",
   ],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
     commonjs: true,
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
-  plugins: ["react", "jest"],
+  plugins: ["jest"],
   globals: {
     process: true,
   },
   rules: {
     "prettier/prettier": "warn",
-    "react/react-in-jsx-scope": 0,
-    "react/jsx-uses-react": 0,
-    "react/prop-types": "off",
   },
 };
