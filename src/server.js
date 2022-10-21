f8d66a630c05e16a9c182d0e530de3f3a58aa053
Yeminson Sanchez <yeminsons@gmail.com>
2022-10-21 16:12:20 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header('Access-Control-Allow-Headers', '*')
   res.header(
